f9f996d8c8ede414efb6277b24042e4c0fe2ee92
JoseRamon <JoseRamon@JoseRamon-PC>
2013-12-18 14:11:44 +0100

    Version 0.02 PRACTICA 3 FASE 2

--- src/sorteo/Sorteo.java
+++ src/sorteo/Sorteo.java
@@ -6,9 +6,14 @@
 
 package sorteo;
 
+import java.awt.Color;
+import java.io.IOException;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -19,6 +24,7 @@ public class Sorteo extends javax.swing.JFrame {
     /**
      * Creates new form Sorteo
      */
+    final int CENT = 3, DECE = 2, VUELTAS = 10, RETARDO_PAR_COMILLAS = 400, RETARDO_PAR_NUM = 500, VUELTAS_PARPADEO = 5, NUMEROS = 10, RETARDO = 150;
     public Sorteo() {
         initComponents();
     }
@@ -40,44 +46,42 @@ public class Sorteo extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jTextFieldNumero = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(1366, 768));
-        getContentPane().setLayout(null);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel1.setFont(new java.awt.Font("Comic Sans MS", 1, 200)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Comic Sans MS", 1, 400)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setBorder(javax.swing.BorderFactory.createEtchedBorder(java.awt.Color.red, java.awt.Color.red));
+        jLabel1.setBorder(javax.swing.BorderFactory.createEtchedBorder(new java.awt.Color(255, 206, 110), new java.awt.Color(255, 206, 110)));
         jLabel1.setOpaque(true);
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(86, 307, 250, 333);
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 190, 300, 500));
 
         jLabel2.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel2.setFont(new java.awt.Font("Comic Sans MS", 1, 200)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Comic Sans MS", 1, 400)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel2.setAlignmentY(0.0F);
-        jLabel2.setBorder(javax.swing.BorderFactory.createEtchedBorder(java.awt.Color.red, java.awt.Color.red));
+        jLabel2.setBorder(javax.swing.BorderFactory.createEtchedBorder(new java.awt.Color(255, 206, 110), new java.awt.Color(255, 206, 110)));
         jLabel2.setOpaque(true);
         jLabel2.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(354, 307, 250, 333);
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 190, 300, 500));
 
         jLabel3.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel3.setFont(new java.awt.Font("Comic Sans MS", 1, 200)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Comic Sans MS", 1, 400)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText(" ");
-        jLabel3.setBorder(javax.swing.BorderFactory.createEtchedBorder(java.awt.Color.red, java.awt.Color.red));
+        jLabel3.setBorder(javax.swing.BorderFactory.createEtchedBorder(new java.awt.Color(255, 206, 110), new java.awt.Color(255, 206, 110)));
         jLabel3.setOpaque(true);
-        getContentPane().add(jLabel3);
-        jLabel3.setBounds(622, 307, 250, 333);
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 190, 300, 500));
 
-        jButton1.setBackground(new java.awt.Color(255, 0, 0));
+        jButton1.setBackground(new java.awt.Color(255, 206, 110));
         jButton1.setFont(new java.awt.Font("Comic Sans MS", 1, 100)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(124, 155, 253));
         jButton1.setText("Gana!");
         jButton1.setBorderPainted(false);
         jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -87,86 +91,120 @@ public class Sorteo extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1);
-        jButton1.setBounds(0, 121, 944, 168);
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(890, 10, 430, 160));
 
-        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 0, 0));
-        jLabel4.setText("IES Los Remedios: ");
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(10, 11, 230, 104);
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 206, 110));
+        jLabel4.setText("IES Los Remedios ");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 10, -1, 104));
 
-        jLabel5.setFont(new java.awt.Font("Tahoma", 2, 24)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(255, 0, 0));
+        jLabel5.setFont(new java.awt.Font("Tahoma", 3, 24)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 206, 110));
         jLabel5.setText("Sorteo Navidad 2013");
-        getContentPane().add(jLabel5);
-        jLabel5.setBounds(246, 11, 237, 104);
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 60, 340, 104));
 
-        jTextFieldNumero.setFont(new java.awt.Font("Tahoma", 1, 10)); // NOI18N
-        getContentPane().add(jTextFieldNumero);
-        jTextFieldNumero.setBounds(839, 11, 95, 19);
+        jTextFieldNumero.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jTextFieldNumero.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        getContentPane().add(jTextFieldNumero, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, 70, 80, 35));
 
-        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel6.setForeground(new java.awt.Color(255, 0, 0));
+        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 206, 110));
         jLabel6.setText("Max:");
-        getContentPane().add(jLabel6);
-        jLabel6.setBounds(808, 13, 27, 14);
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 70, 60, 30));
 
-        jLabel7.setIcon(new javax.swing.ImageIcon("C:\\Users\\JoseRamon\\Desktop\\FP - JAVA\\Sorteo\\imagenes\\esferas-azules-para-navidad-christmas-blue-shine.jpg")); // NOI18N
-        jLabel7.setText("jLabel7");
-        getContentPane().add(jLabel7);
-        jLabel7.setBounds(0, 0, 1370, 770);
+        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/tux-christmas.png"))); // NOI18N
+        jLabel8.setText("jLabel8");
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(1070, 450, 250, 250));
+
+        jLabel7.setIcon(new javax.swing.ImageIcon("C:\\Users\\JoseRamon\\Desktop\\FP - JAVA\\Sorteo\\src\\imagenes\\esferas-azules-para-navidad-christmas-blue-shine.jpg")); // NOI18N
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(-6, 0, 1380, 710));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         try {
-            int numeroMaximo = Integer.valueOf(jTextFieldNumero.getText());
-            Random generador = new Random();
-            String numeroGanador = String.valueOf(generador.nextInt(numeroMaximo)+1);
-            char centenas = '0', decenas = '0', unidades = '0';
-            System.out.println(numeroGanador);
-            if (numeroGanador.length() < 3) {
-                if (numeroGanador.length() < 2) {
-                    unidades = numeroGanador.charAt(0);
-                } else {
-                    decenas = numeroGanador.charAt(0);
-                    unidades = numeroGanador.charAt(1);
-                }
-            } else {
-                centenas = numeroGanador.charAt(0);
-                decenas = numeroGanador.charAt(1);
-                unidades = numeroGanador.charAt(2);
-            }
-            for (int i = 0;i < 10;i++) {
-                int numero = generador.nextInt(10);
-                jLabel1.setText(String.valueOf(numero));
-                if (i == 9)
-                    jLabel1.setText(String.valueOf(centenas));
+            try {
+                int numeroMaximo = Integer.valueOf(jTextFieldNumero.getText());
+                jLabel1.setText("");
+                jLabel2.setText("");
+                jLabel3.setText("");
+                jLabel1.setForeground(Color.WHITE);
+                jLabel2.setForeground(Color.WHITE);
+                jLabel3.setForeground(Color.WHITE);
                 jLabel1.paintImmediately(0,0,jLabel1.getWidth(),jLabel1.getHeight());
-                Thread.sleep(150);
-            }
-            for (int i = 0;i < 10;i++) {
-                int numero = generador.nextInt(10);
-                jLabel2.setText(String.valueOf(numero));
-                if (i == 9)
-                    jLabel2.setText(String.valueOf(decenas));
                 jLabel2.paintImmediately(0,0,jLabel2.getWidth(),jLabel2.getHeight());
-                Thread.sleep(150);
-            }
-            for (int i = 0;i < 10;i++) {
-                int numero = generador.nextInt(10);
-                jLabel3.setText(String.valueOf(numero));
-                if (i == 9)
-                    jLabel3.setText(String.valueOf(unidades));
                 jLabel3.paintImmediately(0,0,jLabel3.getWidth(),jLabel3.getHeight());
-                Thread.sleep(150);
+                Random generador = new Random();
+                String numeroGanador = String.valueOf(generador.nextInt(numeroMaximo)+1);
+                char centenas = '0', decenas = '0', unidades = '0';
+                
+                if (numeroGanador.length() < CENT) {
+                    if (numeroGanador.length() < DECE) {
+                        unidades = numeroGanador.charAt(0);
+                    } else {
+                        decenas = numeroGanador.charAt(0);
+                        unidades = numeroGanador.charAt(1);
+                    }
+                } else {
+                    centenas = numeroGanador.charAt(0);
+                    decenas = numeroGanador.charAt(1);
+                    unidades = numeroGanador.charAt(2);
+                }
+                for (int i = 0;i < VUELTAS;i++) {
+                    int numero = generador.nextInt(NUMEROS);
+                    jLabel1.setText(String.valueOf(numero));
+                    if (i == VUELTAS - 1)
+                        jLabel1.setText(String.valueOf(centenas));
+                    jLabel1.paintImmediately(0,0,jLabel1.getWidth(),jLabel1.getHeight());
+                    Thread.sleep(RETARDO);
+                }
+                for (int i = 0;i < VUELTAS;i++) {
+                    int numero = generador.nextInt(NUMEROS);
+                    jLabel2.setText(String.valueOf(numero));
+                    if (i == VUELTAS - 1)
+                        jLabel2.setText(String.valueOf(decenas));
+                    jLabel2.paintImmediately(0,0,jLabel2.getWidth(),jLabel2.getHeight());
+                    Thread.sleep(RETARDO);
+                }
+                for (int i = 0;i < VUELTAS;i++) {
+                    int numero = generador.nextInt(NUMEROS);
+                    jLabel3.setText(String.valueOf(numero));
+                    if (i == VUELTAS - 1)
+                        jLabel3.setText(String.valueOf(unidades));
+                    jLabel3.paintImmediately(0,0,jLabel3.getWidth(),jLabel3.getHeight());
+                    Thread.sleep(RETARDO);
+                }
+                Color colorLetras = new Color(255,206,110);
+                jLabel1.setForeground(colorLetras);
+                jLabel2.setForeground(colorLetras);
+                jLabel3.setForeground(colorLetras);
+                for (int i = 0; i < VUELTAS_PARPADEO; i++) {
+                    jLabel1.setText("");
+                    jLabel1.paintImmediately(0,0,jLabel1.getWidth(),jLabel1.getHeight());
+                    jLabel2.setText("");
+                    jLabel2.paintImmediately(0,0,jLabel2.getWidth(),jLabel2.getHeight());
+                    jLabel3.setText("");
+                    jLabel3.paintImmediately(0,0,jLabel3.getWidth(),jLabel3.getHeight());
+                    Thread.sleep(RETARDO_PAR_COMILLAS);
+                    jLabel1.setText(String.valueOf(centenas));
+                    jLabel1.paintImmediately(0,0,jLabel1.getWidth(),jLabel1.getHeight());
+                    jLabel2.setText(String.valueOf(decenas));
+                    jLabel2.paintImmediately(0,0,jLabel2.getWidth(),jLabel2.getHeight());
+                    jLabel3.setText(String.valueOf(unidades));
+                    jLabel3.paintImmediately(0,0,jLabel3.getWidth(),jLabel3.getHeight());
+                    Thread.sleep(RETARDO_PAR_NUM);
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Sorteo.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Sorteo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NumberFormatException e) {
+            try {
+                JOptionPane.showMessageDialog(this,"No insertaste numero maximo.","Sorteo 2013",JOptionPane.WARNING_MESSAGE, new ImageIcon(ImageIO.read(getClass().getResource("/imagenes/santa-evil1.jpg"))));
+            } catch (IOException ex) {
+                Logger.getLogger(Sorteo.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -213,6 +251,7 @@ public class Sorteo extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JTextField jTextFieldNumero;
     // End of variables declaration//GEN-END:variables
 }
